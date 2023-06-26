8860d6dddd2e3a108d98eac124b23030d88533f9
Chase Palmer <chase.palmer012704@gmail.com>
2023-06-26 14:28:55 -0400

    Added mongoose connection to application

--- server.js
+++ server.js
@@ -1,2 +1,21 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+
+const uri = 'mongodb+srv://chasepalmer1:Chalmer619@ccbfishing.0rfh64f.mongodb.net/?retryWrites=true&w=majority'
+
+async function connect() {
+    try {
+        await mongoose.connect(uri);
+        console.log("Connected to MongoDB");
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+connect();
+
+app.listen('3000', () => {
+    console.log('Server started.');
+});
+
