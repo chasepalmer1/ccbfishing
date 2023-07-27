c57d04e6bdfef6bcccb1733ed1f839a0fa86cac5
Chase Palmer <chase.palmer012704@gmail.com>
2023-07-27 17:47:45 -0400

    Fixed image in About Us size issue

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -5,7 +5,7 @@ const Profile = (props) => {
     
     return (
         <div className="profile">
-            <img className="image" style={{height: "70%",objectFit: "contain", border: "solid black 1px", borderRadius: "5px"}} src={process.env.PUBLIC_URL + "/resources/" + props.src} alt={props.alt}/>
+            <img className="image" style={{height: "70%",objectFit: "contain", borderRadius: "5px"}} src={process.env.PUBLIC_URL + "/resources/" + props.src} alt={props.alt}/>
             <h3 style={{marginTop: "5%"}}><em>{props.name}</em></h3>
             <svg id="ccb-insta" href={"https://instagram.com/ccb.fishing?igshid=NTc4MTIwNjQ2YQ=="} target="_blank" xmlns="http://www.w3.org/2000/svg" width="max(10%, 20px)" height="max(10%, 20px)" viewBox="0 0 48 48">
                 <g fill="none"><path stroke="currentColor" strokeLinejoin="round" strokeWidth="4" style={{marginLeft: "0"}}
