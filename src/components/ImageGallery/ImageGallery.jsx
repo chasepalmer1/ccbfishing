a5be025fd95b0952b815f173e798f3ac476a352c
Chase Palmer <chase.palmer012704@gmail.com>
2023-08-12 19:05:00 -0400

    Moved images to src folder and redid directory paths to match the move

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -5,22 +5,22 @@ const ImageGallery = (props) => {
 
     function loadImages(name) {
         if (name === 'fishing') {
-            const images = require.context("../../../public/resources/fishing", true);
+            const images = require.context("../../images/fishing", true);
             const imageArray = images.keys().map(image => images(image));
             return imageArray;
         }
         if (name === 'offroading') {
-            const images = require.context("../../../public/resources/offroading", true);
+            const images = require.context("../../images/offroading", true);
             const imageArray = images.keys().map(image => images(image));
             return imageArray;
         }
         if (name === 'camping') {
-            const images = require.context("../../../public/resources/camping", true);
+            const images = require.context("../../images/camping", true);
             const imageArray = images.keys().map(image => images(image));
             return imageArray;
         }
         if (name === 'projects') {
-            const images = require.context("../../../public/resources/projects", true);
+            const images = require.context("../../images/projects", true);
             const imageArray = images.keys().map(image => images(image));
             return imageArray;
         }
